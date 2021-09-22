82781f4198c2c3ef37968202575eb15df4bb99b9
Sitakkun <t5k1m7.swim@gmail.com>
2021-09-22 19:45:14 +0900

    投稿のリンクの表示を改善

--- src/components/layout.js
+++ src/components/layout.js
@@ -55,21 +55,21 @@ const Layout = ({ children }) => {
     // テーマ作成
     const theme = createTheme({
         typography: {
-        fontFamily: [
-            "Noto Sans JP",
-            "Lato",
-            "游ゴシック Medium",
-            "游ゴシック体",
-            "Yu Gothic Medium",
-            "YuGothic",
-            "ヒラギノ角ゴ ProN",
-            "Hiragino Kaku Gothic ProN",
-            "メイリオ",
-            "Meiryo",
-            "ＭＳ Ｐゴシック",
-            "MS PGothic",
-            "sans-serif",
-        ].join(","),
+            fontFamily: [
+                "Noto Sans JP",
+                "Lato",
+                "游ゴシック Medium",
+                "游ゴシック体",
+                "Yu Gothic Medium",
+                "YuGothic",
+                "ヒラギノ角ゴ ProN",
+                "Hiragino Kaku Gothic ProN",
+                "メイリオ",
+                "Meiryo",
+                "ＭＳ Ｐゴシック",
+                "MS PGothic",
+                "sans-serif",
+            ].join(","),
         },
         palette: {
             primary: { main: colors.blue[800] }, // テーマの色
